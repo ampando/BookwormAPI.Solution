8499e2c9392d0267cfc7e19a53e465cd379c4128
RosarioRu <mariarruval@gmail.com>
2022-09-18 15:22:20 -0700

    Update models with library class.
    Co-authored-by: Adrienne Matosich <amatosich@gmail.com>
    Co-authored-by: Ryan Broadsword <rbroadsword@gmail.com>
    Co-authored-by: Sean Julius <crimsonsand@live.com>

--- BookwormAPI/Models/Library.cs
+++ BookwormAPI/Models/Library.cs
@@ -2,31 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-
 namespace BookwormAPI.Models
 {
-    public class User
+    public class Library
     {
-        public int UserId { get; set; }
-
+        public int LibraryId { get; set; }
+        public virtual ICollection<Review> Reviews {get; set;}
+        public virtual ICollection<Rating> Ratings {get; set;}
         [Required]
         public string Name { get; set; }
         
 
-        public User()
+        public Library()
         {
           this.Reviews = new HashSet<Review>();
           this.Ratings = new HashSet<Rating>();
-        }
-
-        public virtual ICollection<Review> Reviews {get; set;}
-        public virtual ICollection<Rating> Ratings {get; set;}
-
+        }     
+        //public virtual ApplicationUser User { get; set; }
     }
 }
 
 
-//library class below (from web app client side)
+/*library class below (from web app client side)
 public class Library
   { 
     public Library()
@@ -37,7 +34,5 @@ public class Library
     public int LibraryId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<BookLibrary> JoinEntities { get; set; }
-    
-    public virtual ApplicationUser User { get; set; }
-  }
-}
+    }
+  }*/
